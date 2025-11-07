0d63ff338f9a8a52e23bc06b0617132690dd92e9
ammonmerrell <amerrell@byupathway.edu>
2025-11-06 18:57:50 -0700

    commented renderlist in product.mjs

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -12,11 +12,11 @@ export default class ProductList{
         this.renderList(list);
     }
 
-  renderList(list) {
-    const htmlStrings = list.map(productCardTemplate);
-    this.listElement.insertAdjacentHTML("afterbegin", htmlStrings.join(""));
-        renderListWithTemplate(productCardTemplate, this.listElement, list);
-    }
+  // renderList(list) {
+  //   const htmlStrings = list.map(productCardTemplate);
+  //   this.listElement.insertAdjacentHTML("afterbegin", htmlStrings.join(""));
+  //       renderListWithTemplate(productCardTemplate, this.listElement, list);
+  //   }
 
 }
 
