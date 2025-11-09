7441567abbf6e58f70de6c709621880cbe69d34b
ammonmerrell <amerrell@byupathway.edu>
2025-11-08 19:36:05 -0700

    commited the code from my branch.

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,4 +1,8 @@
-import { getLocalStorage, renderListWithTemplate, setLocalStorage } from "./utils.mjs";
+import {
+  getLocalStorage,
+  renderListWithTemplate,
+  setLocalStorage,
+} from "./utils.mjs";
 
 let totalPrice = 0;
 let arrayIndex = -1;
@@ -35,42 +39,40 @@ renderCartContents();
 document.querySelector(".cart-total").textContent = "Total: $" + totalPrice;
 
 function checkId(evt) {
-
-
   let cart = JSON.parse(localStorage.getItem("so-cart"));
-  cart.forEach(element => {
+  cart.forEach((element) => {
     if (element.Id === evt.target.id) {
       // console.log(element)
       // element.innerHTML = "";
       // setLocalStorage("so.cart", element)
       // renderListWithTemplate(element, this.element, cart)
 
-      console.log("element matches!")
-
+      console.log("element matches!");
 
       // The following code is the add item to so-cart.
-      console.log(cart.length)
-      cart = cart.filter(i => i.Id != evt.target.id)
-      console.log(cart.length)
+      console.log(cart.length);
+      cart = cart.filter((i) => i.Id != evt.target.id);
+      console.log(cart.length);
       setLocalStorage("so-cart", cart);
-      console.log(cart)
-      renderCartContents()
-      cartItemTemplate(cart)
-      
-      document.querySelector(".cart-total").textContent = "Total: $" + totalPrice;
+      console.log(cart);
+      renderCartContents();
+      cartItemTemplate(cart);
 
+      document.querySelector(".cart-total").textContent =
+        "Total: $" + totalPrice;
 
       // addProductToCart();
-      
+
       // function addProductToCart() {
       //   const cartItems = getLocalStorage("so-cart") || [];
       //   cartItems.push(element);
       //   setLocalStorage("so-cart", cartItems);
       // };
-
     }
   });
 }
 
-const listItems = document.querySelector(".product-list").addEventListener("click", checkId)
+const listItems = document
+  .querySelector(".product-list")
+  .addEventListener("click", checkId);
 const an = document.querySelector(".cart-card divider");
