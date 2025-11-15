a6e25d23f343fe702e4b65acc5391b5ecd869e9c
FaithIdowu123 <ofaith@byupathway.edu>
2025-11-15 21:39:59 +0100

    fix

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,7 @@
 import ProductData from "./ProductData.mjs";
 import ProductList from "./ProductList.mjs";
 import { loadHeaderFooter } from "./utils.mjs";
+import Alert from "./alert.mjs";
 
 const dataSource = new ProductData();
 const element = document.querySelector(".product-list");
@@ -9,3 +10,4 @@ loadHeaderFooter();
 const productList = new ProductList("tents", dataSource, element);
 
 productList.init();
+Alert.loadAndShowAlerts();
